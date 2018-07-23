9d2ea0c0d6f19e70cc17d35c5c294a6603b3b954
DESKTOP-NAUUI9R\Gleb <trulyirishdancer@yandex.ru>
2018-07-23 11:04:08 +0300

    ci: add JSDoc to service code
    JSDoc has been added successfully
    
    Closes #15

--- src/handlePullRequest.js
+++ src/handlePullRequest.js
@@ -1,25 +1,27 @@
-const helper = require("./helper.js");
-
+const helper = require('./helper.js');
+/**
+ * Catches webhook from Github after pull request opening, reopening
+ * or editing; clones changes from Github, launches analysis and, finally,
+ * sets status of the pull request according to the results of analysis.
+ * @param  {object} context - context got by listening to Github webhooks,
+ * belongs to the pull request that caused launch of the function.
+ */
 function handlePullRequest(context) {
-  const task = helper.setStatus(context, "pending");
+  const task = helper.setStatus(context, 'pending');
 
-  Promise.all([task]).then(data => {
+  Promise.all([task]).then((data) => {
     const { title, html_url: htmlUrl, head } = context.payload.pull_request;
 
     const workingDirectory = helper.getWorkingDirectory(head.sha);
 
     helper.runGit(context, head.repo.full_name, head.ref, workingDirectory);
 
-    const errors = helper.runAnalysis(context, workingDirectory);
+    const errors = helper.runAnalysis(workingDirectory);
 
-    helper.deleteWorkingDirectory(workingDirectory + "\\**");
+    helper.deleteWorkingDirectory(workingDirectory + '\\**');
 
-    if (errors) {
-      helper.setStatus(context, "error");
-    }
-    else {
-      helper.setStatus(context, "success");
-    }
-  })
+    errors ? helper.setStatus(context, 'error') : helper.setStatus(context, 'success');
+
+  });
 }
 module.exports = handlePullRequest;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 module.exports = littleSmart;
 
 const handlePullRequest = require('./handlePullRequest');
-
+/**
+ * Marks webhooks to listen to and listens to them.
+ * @param  {Application} robot - an instance of Application. Gives the
+ * service access to all of the GitHub webhooks.
+ */
 function littleSmart(robot) {
   robot.on([
     'pull_request.opened',
