3b8860705a23c668ec56d18a1e8b9b15208abd18
DESKTOP-NAUUI9R\Gleb <trulyirishdancer@yandex.ru>
2018-07-23 11:04:08 +0300

    ci: add JSDoc to service code
    JSDoc has been added successfully
    
    Closes #15

--- src/cmdWrapper.js
+++ src/cmdWrapper.js
@@ -1,20 +1,27 @@
-const fs = require("fs");
-const exec = require("child_process").execSync;
+const exec = require('child_process').execSync;
 
-const cmdWrapperModule = (function () {
+const cmdWrapperModule = (function() {
+	/**
+ 	* Launches a child process, supervising results of its work.
+ 	*@param {string} commander - Commands to be executed via command line
+	*@param {string} commanderArguments - Argumends passed to the commands to
+	specify their work
+	*@param {string} workingDirectory - Path to the directory, which is necessary
+	to know to execute commands.
+	*@return {number}
+	*/
 	function runExecutable(commander, commanderArguments, workingDirectory) {
 		try {
 			const cmd = commander + commanderArguments + workingDirectory;
 			exec(cmd);
 			return 0;
-		}
-		catch (err) {
+		} catch (err) {
 			console.log(`An error occured: ${err}`);
 			return 1;
 		}
 	}
 	return {
-		runExecutable
+		runExecutable,
 	};
 }());
 module.exports = cmdWrapperModule;
--- src/helper.js
+++ src/helper.js
@@ -1,82 +1,108 @@
-const del = require("del");
-const mkdirp = require("mkdirp");
-const path = require("path");
-const process = require("process");
+const del = require('del');
+const mkdirp = require('mkdirp');
+const path = require('path');
+const process = require('process');
 const cmdWrapper = require('./cmdWrapper');
-const configuration = require('./config.json')
+const configuration = require('./config.json');
 
-var helperModule = (function () {
+const helperModule = (function() {
+	/**
+	 * Creates a new directory in a current working directory if
+	 * it does not exist already and names it with sha of the pull request
+	 * which caused launch of the function.
+	 * @param  {string} sha - sha of the pull request which caused launch
+	 * of the function,
+	 * @return {string}
+	 */
 	const getWorkingDirectory = (sha) => {
 		const workingDirectory = path.join(process.cwd(), sha);
 
 		try {
 			mkdirp.sync(workingDirectory);
-		}
-		catch (err) {
+		} catch (err) {
 			console.log(`An error occured: ${err}`);
-			setStatus(context, "error");
+			setStatus(context, 'error');
 		}
 
 		return workingDirectory;
 	};
-
+	/**
+	 * Deletes the directory passed to the function with all its contents.
+	 * @param  {string} workingDirectory - the directory to be deleted.
+	 */
 	const deleteWorkingDirectory = (workingDirectory) => {
 		try {
 			del.sync(workingDirectory);
-		}
-		catch (err) {
+		} catch (err) {
 			console.log(`An error occured: ${err}`);
-			setStatus(context, "error");
+			setStatus(context, 'error');
 		}
 	};
-
+	/**
+	 * Clones pull request that caused launch of the function
+	 * @param  {object} context - context got by listening to Github webhooks,
+ 	 * belongs to the pull request that caused launch of the function.
+	 * @param  {string} gitUrl - URL of the repository, which contains information
+	 * from the pull request that caused launch of the function.
+	 * @param  {string} gitBranch - Specific branch of the repository,
+	 * which contains information from the pull request that caused
+	 * launch of the function.
+	 * @param  {string} workingDirectory - The directory to clone repository
+	 * branch to.
+	 */
 	const runGit = (context, gitUrl, gitBranch, workingDirectory) => {
 		try {
 			gitUrl = `https://github.com/${gitUrl}`;
-			const result = cmdWrapper.runExecutable("git clone ", ` -b ${gitBranch} ${gitUrl} `, workingDirectory);
-		}
-		catch (err) {
+			cmdWrapper.runExecutable('git clone ',
+				` -b ${gitBranch} ${gitUrl} `, workingDirectory);
+		} catch (err) {
 			console.log(`An error occured: ${err}`);
 			deleteWorkingDirectory(workingDirectory);
-			setStatus(context, "error");
+			setStatus(context, 'error');
 		}
 	};
-
-	const runAnalysis = (cotext, workingDirectory) => {
+	/**
+	 * Launches analysis of the cloned repository branch.
+	 * @param  {string} workingDirectory - The directory, containing
+	 * cloned repository branch.
+	 * @return {number}
+	 */
+	const runAnalysis = (workingDirectory) => {
 		try {
-			const result = cmdWrapper.runExecutable("jshint ", "", workingDirectory);
+			const result = cmdWrapper.runExecutable('jshint ', '', workingDirectory);
 			return result;
-		}
-		catch (err) {
+		} catch (err) {
 			console.log(`An error occured: ${err}`);
 			deleteWorkingDirectory(workingDirectory);
-			setStatus(context, "error");
+			setStatus(context, 'error');
 		}
 	};
-
+	/**
+	 * Sends required status of the pull request to Github
+	 * @param  {object} context - context got by listening to Github webhooks,
+ 	 * belongs to the pull request that caused launch of the function.
+	 * @param  {string} statusArgument - status of the pull request to
+	 * be sent to Github.
+	 * @return {Function}
+	 */
 	const setStatus = (context, statusArgument) => {
-		const { title, html_url: htmlUrl, head } = context.payload.pull_request;
-		let information = configuration.statuses[statusArgument];
-		if (!information) {
-			console.log("An error occured: wrong status");
-			statusArgument = "error";
-			information = configuration.statuses[statusArgument];
-		}
+		const {title, html_url: htmlUrl, head} = context.payload.pull_request;
+		const information = configuration.statuses[statusArgument];
 		return context.github.repos.createStatus(context.repo({
 			sha: head.sha,
 			state: statusArgument,
 			target_url: '',
 			description: information,
-			context: 'little&smart'
+			context: 'little&smart',
 		}));
-	}
+	};
 
 	return {
 		getWorkingDirectory,
 		deleteWorkingDirectory,
 		runGit,
 		runAnalysis,
-		setStatus
+		setStatus,
 	};
 }());
 module.exports = helperModule;
